b6fbb86b3165673c138507391b70fa03af77c00c
Steven Beaty <beatys@ms16142.local>
2017-02-01 17:53:22 -0700

    More tests.

--- src/test/java/edu/msudenver/cs/JCLO/TestJCLO.java
+++ src/test/java/edu/msudenver/cs/JCLO/TestJCLO.java
@@ -2,6 +2,9 @@ import edu.msudenver.cs.jclo.JCLO;
 
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 class JCLOArgs
 {
@@ -21,16 +24,35 @@ class JCLOArgs
 
 public class TestJCLO
 {
+
     @Test
     public void double_dash()
     {
+        HashMap<String, Boolean> hm = new HashMap<String, Boolean>();
+        hm.put ("", false);
+        hm.put ("--debug", true);
+        hm.put ("--debug=FALSE", false);
+	    hm.put ("--debug=true", true);
+	    hm.put ("--debug=yes", true);
+	    hm.put ("--debug=YES", true);
+	    hm.put ("--debug=false", false);
+	    hm.put ("--debug=no", false);
+	    hm.put ("--debug=NO", false);
+	    // hm.put ("--debug", "one", "two", "three");
+
         JCLOArgs jcloargs = new JCLOArgs();
         JCLO jclo = new JCLO (jcloargs);
 
-        jclo.parse (new String[]{"--debug"});
-        Assert.assertEquals (jcloargs.debug, true);
+        Iterator iterator = hm.entrySet().iterator();
+        while(iterator.hasNext())
+        {
+            Map.Entry me = (Map.Entry) iterator.next();
+            String foo[] = new String[1];
+            foo[0] = (String) me.getKey();
 
-        jclo.parse (new String[]{"--debug=FALSE"});
-        Assert.assertEquals (jcloargs.debug, false);
+            jclo.parse (foo);
+
+            Assert.assertEquals (jcloargs.debug, me.getValue());
+        }
     }
 }
